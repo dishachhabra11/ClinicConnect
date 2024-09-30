4b4589f7e77ab984a9721b844d0aa1394804c093
disha <dishachhabra173@gmail.com>
2024-09-30 18:42:53 +0530

    commit

--- frontend/src/components/InputFields/PasswordField.jsx
+++ frontend/src/components/InputFields/PasswordField.jsx
@@ -6,13 +6,13 @@ const PasswordField = ({
   placeholder = "Enter your password", // Default placeholder
   borderColor = "border-light-gray", // Default border color class
   textColor = "text-dark-gray", // Default text color class
-    customClass = "", // Allow passing custom class names if needed
+  customClass = "", // Allow passing custom class names if needed
   label, // Label for the input
 }) => {
   return (
     <div className="w-full mb-4">
       <label htmlFor="email" className=" text-gray-700 font-poppins  font-semibold mb-3 text-sm">
-       {label}
+        {label}
       </label>
       <input type="password" value={value} onChange={onChange} className={`w-full px-3 py-2 border rounded-lg outline-none font-roboto ${textColor} ${borderColor} ${customClass}`} placeholder={placeholder} required />
     </div>
